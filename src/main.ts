1ee5eafa5a419dae9c406c17e3bcedfe75d4de59
Krzysztof <krisczajka@gmail.com>
2024-02-08 12:28:26 +0100

    Add firebase configuration

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,19 @@ import "@src/style.css";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 import vClickOutside from "click-outside-vue3";
-
+import { initializeApp } from "firebase/app";
 import App from "@src/App.vue";
 
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain:import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
 const pinia = createPinia();
+initializeApp(firebaseConfig);
 
 createApp(App).use(pinia).use(router).use(vClickOutside).mount("#app");
